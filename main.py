3321e95d61eac695c0359e0024ff3ce7df0b8fe7
sofianechaoui <sofiane.chaoui@vestalis.fr>
2025-02-25 17:14:20 +0100

    finish part 7

--- main.py
+++ main.py
@@ -19,41 +19,45 @@ def main():
           "6: status of programmer\n")
 
     command = input("command: ")
+
     while command != "0":
-        if command == "1":
-            description = input("description: ")
-            programmer_name, estimated_time = input("programmer and workload estimate: ").split()
-            orders.add_order(description, programmer_name, int(estimated_time))
-            print("added!")
-        elif command == "2":
-            if not orders.all_orders(True):
-                print("no finished tasks")
-            for order in orders.all_orders(True):
-                print(order)
-        elif command == "3":
-            if not orders.all_orders():
-                print("no unfinished tasks")
-            for order in orders.all_orders():
-                print(order)
-        elif command == "4":
-            order_id = input("id: ")
-            orders.mark_finished(int(order_id))
-            print("marked as finished!")
-        elif command == "5":
-            if not orders.programmers():
-                print("no programmers")
-            for programmers in orders.programmers():
-                print(programmers)
-        elif command == "6":
-            programmer_name = input("programmer: ")
-            status = orders.status_of_programmer(programmer_name)
-            print(f"task : finished {status[0]},",
-                  f"not finished {status[1]},",
-                  f"hours: done {status[2]},",
-                  f"scheduled {status[3]}")
-        else:
-            print(f"'{command}' command not exist")
-        command = input("command: ")
+        try:
+            if command == "1":
+                description = input("description: ")
+                programmer_name, estimated_time = input("programmer and workload estimate: ").split()
+                orders.add_order(description, programmer_name, int(estimated_time))
+                print("added!")
+            elif command == "2":
+                if not orders.all_orders(True):
+                    print("no finished tasks")
+                for order in orders.all_orders(True):
+                    print(order)
+            elif command == "3":
+                if not orders.all_orders():
+                    print("no unfinished tasks")
+                for order in orders.all_orders():
+                    print(order)
+            elif command == "4":
+                order_id = input("id: ")
+                orders.mark_finished(int(order_id))
+                print("marked as finished!")
+            elif command == "5":
+                if not orders.programmers():
+                    print("no programmers")
+                for programmers in orders.programmers():
+                    print(programmers)
+            elif command == "6":
+                programmer_name = input("programmer: ")
+                status = orders.status_of_programmer(programmer_name)
+                print(f"task : finished {status[0]},",
+                    f"not finished {status[1]},",
+                    f"hours: done {status[2]},",
+                    f"scheduled {status[3]}")
+            else:
+                print(f"'{command}' command not exist")
+            command = input("command: ")
+        except ValueError:
+            print("erroneous input")
 
 
 if __name__ == "__main__":
