315e84d41709f4e605db7aa8b2fb70b61263245a
sofianechaoui <sofiane.chaoui@vestalis.fr>
2025-02-25 16:44:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,108 @@
+from task import OrderBook
+import os
+
+
+def main():
+    orders = OrderBook()
+    # clear the terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(" ====================================================\n",
+          "========= welcome to the DSTI task manager =========\n",
+          "====================================================\n\n",
+          "enter the command you want to execute :)\n",
+          "0: exit\n",
+          "1: add order\n",
+          "2: list finished tasks\n",
+          "3: list unfinished tasks\n",
+          "4: mark tasks as finished\n",
+          "5: programmers\n",
+          "6: status of programmer\n")
+
+    command = input("command: ")
+    while command != "0":
+        if command == "1":
+            description = input("description: ")
+            programmer_name, estimated_time = input("programmer and workload estimate: ").split()
+            orders.add_order(description, programmer_name, int(estimated_time))
+            print("added!")
+            for order in orders.all_orders():
+                print(order)
+            # orders.add_order(description, programmer_name, estimated_time)
+        elif command == "2":
+            pass
+            # for order in orders.all_orders():
+            #     print(order)
+        elif command == "3":
+            pass
+            # for order in orders.all_orders():
+            #     print(order)
+        elif command == "4":
+            pass
+            # order_id = int(input("order id: "))
+            # orders.mark_finished(order_id)
+        elif command == "5":
+            pass
+            # for programmer in orders.programmers():
+            #     print(programmer)
+        elif command == "6":
+            pass
+            # programmer_name = input("programmer name: ")
+            # status = orders.status_of_programmer(programmer_name)
+            # print(status)
+        else:
+            print(f"'{command}' command not exist")
+        command = input("command: ")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# orders = OrderBook()
+# orders.add_order("create the backend + BDD", "Abi", 5)
+# orders.add_order("make cofee for John", "Abi", 3)
+# orders.add_order("analyse and clean the datasets", "John", 10)
+# orders.add_order("create the ML", "John", 20)
+# orders.add_order("make cofee for Abi", "John", 30)
+# orders.add_order("deploy the application", "Abi", 2)
+
+# for order in orders.all_orders():
+#     print(order)
+
+# # task 2
+# for programmers in orders.programmers():
+#     print(programmers)
+
+# dict_orders = orders.dictionnary()
+# print(dict_orders['Abi'])
+
+# # print("Marking task finished")
+# orders.mark_finished(1)
+# # orders.mark_finished(2)
+# orders.mark_finished(3)
+# # orders.mark_finished(4)
+
+# # print("all unfinished tasks")
+# # for order in orders.all_orders():
+# #     print(order)
+
+# status = orders.status_of_programmer("John")
+# print(status)
---
+++ task.py
@@ -0,0 +1,90 @@
+"""module to manage tasks"""
+
+
+class Task:
+    """PART 1 class to instantiate a task"""
+    id = 0
+
+    def __init__(self, description: str = '', estimated_time: int = 0,
+                 programmer_name: str = '', status: bool = False):
+        Task.id += 1
+        self.id = Task.id
+        self.description = description
+        self.programmer_name = programmer_name
+        # logic to add good format for estimated_time
+        self.estimated_time = estimated_time
+        # logic to add good format for status
+        self.status = "FINISHED" if status else "NOT FINISHED"
+
+    def __str__(self):
+        return (f"{self.id}: {self.description}, ({self.estimated_time} hours)"
+                f", {self.programmer_name}, {self.status}")
+
+
+class OrderBook:
+    """PART2 class to manage orders"""
+
+    def __init__(self):
+        self.orders = []
+
+    def add_order(self, description: str,  programmer_name: str,
+                  estimated_time: int, status: bool = False) -> None:
+        """PART2 fuction to add"""
+        task = Task(description, estimated_time, programmer_name, status)
+        self.orders.append(task)
+
+    def all_orders(self):
+        """PART2 function return all orders"""
+        # return all false status tasks
+        result_order = []
+        for order in self.orders:
+            if order.status == "NOT FINISHED":
+                result_order.append(order)
+        return result_order
+
+    def programmers(self):
+        """PART2 function return all programmers"""
+        return [order.programmer_name for order in self.orders]
+
+    def dictionnary(self):
+        """PART3 function return all orders in a dictionnary key=programmer"""
+        result_dict = {}
+        # enrich dict
+        for order in self.all_orders():
+            if order.programmer_name not in result_dict:
+                result_dict[order.programmer_name] = []
+            result_dict[order.programmer_name].append(
+                f"{order.id}: {order.description},"
+                f"{order.estimated_time}, {order.status}"
+            )
+        return result_dict
+
+    def mark_finished(self, order_id: int):
+        """PART4 function to update the status to FINISHED"""
+        for order in self.orders:
+            if order.id == order_id:
+                order.status = "FINISHED"
+                print(order)
+                return order
+        raise ValueError("Task not found")
+
+    def status_of_programmer(self, programmer_name: str):
+        """PART5 function return tuple sum of unfinish/finish hours"""
+        # all hours of programmer
+        finish_hours = 0
+        unfinish_hours = 0
+        # number of tasks
+        finish_tasks = 0
+        unfinish_tasks = 0
+        result = ()
+        # enrich hours anf tasks
+        for order in self.orders:
+            if order.programmer_name == programmer_name:
+                if order.status == "FINISHED":
+                    finish_hours += order.estimated_time
+                    finish_tasks += 1
+                else:
+                    unfinish_hours += order.estimated_time
+                    unfinish_tasks += 1
+        result = (finish_tasks, unfinish_tasks, finish_hours, unfinish_hours)
+        return result
